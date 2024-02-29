d0d1bb16d2f9bfc32a884e10ce43430a5ca3ad50
jibijohndavid <jibijohndavid@gmail.com>
2024-02-29 20:18:11 +0530

    removed auto generate id

--- src/app/api/product/add/route.ts
+++ src/app/api/product/add/route.ts
@@ -3,18 +3,15 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 import { AddProductInput, AddProductSchema } from "./AddProductSchema";
-import { customAlphabet } from "nanoid/async";
 
 export async function POST(req: Request) {
   try {
     const body = (await req.json()) as AddProductInput;
     const data = AddProductSchema.parse(body);
-    const generateCode = customAlphabet("1234567890ABCDEFG", 6);
 
     await prisma.product.create({
       data: {
         ...data,
-        code: await generateCode(),
       },
     });
 
